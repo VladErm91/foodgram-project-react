7ee72835cfaef2deb35834b13cf3780930ce84bd
Влад Ермолаев <97435874+VladErm91@users.noreply.github.com>
2023-09-02 15:45:05 +0300

    Update settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,11 +1,10 @@
 # flake8: noqa
 import os
 from dotenv import load_dotenv
-from pathlib import Path
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-value')
 
