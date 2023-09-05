6865d44f081c5c0680d4a6f9542bda8c48e465b9
Влад Ермолаев <97435874+VladErm91@users.noreply.github.com>
2023-09-05 21:55:05 +0300

    improvements after review

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -15,9 +15,9 @@ class Command(BaseCommand):
                   ) as data_file_ingredients:
             ingredient_data = json.loads(data_file_ingredients.read())
             Ingredient.objects.bulk_create(
-                  Ingredient(**ingredients)
-                  for ingredients in ingredient_data
-                  )
+                Ingredient(**ingredients)
+                for ingredients in ingredient_data
+                )
 
         with open(settings.TAGS_PATH, encoding='utf-8',
                   ) as data_file_tags:
